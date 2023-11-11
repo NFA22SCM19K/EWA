b6496010bc2bb80325ade318096010c1c652b903
NFA22SCM19K <115971820+NFA22SCM19K@users.noreply.github.com>
2023-11-11 13:34:34 -0800

    Update App.js

--- smarthomes/src/App.js
+++ smarthomes/src/App.js
@@ -12,11 +12,30 @@ import AccessoryList from './AccessoryList';
 import Registration from './Registration';
 import Cart from './Cart';
 import Details from './Details';
+import HomeSM from './HomeSM';
+import HomeSAM from './HomeSAM';
+import ViewOrder from './ViewOrder';
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
   const [detailsItem, setDetailsItems] = useState([]);
 
+  const userDetails = {
+    user1: { password: 'password1', usertype: 'customer' },
+    user2: { password: 'password2', usertype: 'storeManager' },
+    user3: { password: 'password3', usertype: 'saleManager' },
+    test1: { password: '123', usertype: 'customer'},
+    test2: { password: '123', usertype: 'customer'},
+    // Add more users as needed
+  };
+
+  const paymentDetails = {
+    1:  [{userName: "user1", orderName: "Echo Dot (5th Gen, 2022 release) with clock", orderPrice: 59.99, firstName:"test", creditCardNo: 7456},
+         {userName: "user1", orderName: "Google - Nest Learning Smart Wifi Thermostat", orderPrice: 249.99, firstName:"test", creditCardNo: 7456},
+        ],
+    2:  [{ userName: "test2", orderName: "Echo Dot (5th Gen, 2022 release) with clock", orderPrice: 59.99, firstName:"test2", creditCardNo: 1235},
+        ],
+  }
 
   const handleDetails = (item)=>{
     setDetailsItems(item)
@@ -44,35 +63,44 @@ function App() {
         <Route exact path="/home">
           <Home />
         </Route>
+        <Route exact path="/homesm">
+          <HomeSM />
+        </Route>
+        <Route exact path="/homesam">
+          <HomeSAM />
+        </Route>
         <Route path="/DoorBellsList">
           <DoorBellsList addItem={handleProduct}  details={handleDetails}/>
         </Route>
         <Route path="/DoorLocksList">
-          <DoorLocksList />
+          <DoorLocksList addItem={handleProduct}  details={handleDetails}/>
         </Route>
         <Route path="/SpeakersList">
-          <SpeakersList />
+          <SpeakersList addItem={handleProduct}  details={handleDetails}/>
         </Route>
         <Route path="/LightingsList">
-          <LightingsList />
+          <LightingsList addItem={handleProduct}  details={handleDetails}/>
         </Route>
         <Route path="/ThermostatsList">
-          <ThermostatsList />
+          <ThermostatsList addItem={handleProduct}  details={handleDetails}/>
         </Route>
         <Route path="/AccessoryList">
-          <AccessoryList />
+          <AccessoryList addItem={handleProduct}  details={handleDetails}/>
         </Route>
         <Route exact path="/Login">
-          <Login />
+          <Login userDetails = {userDetails}/>
         </Route>
         <Route path="/Registration">
-          <Registration />
+          <Registration userDetails = {userDetails}/>
         </Route>
         <Route exact path="/Cart">
           <Cart cartItems = {cartItems}/>
         </Route>
         <Route exact path="/Details">
           <Details item = {detailsItem}/>
+        </Route>
+        <Route exact path="/ViewOrder">
+          <ViewOrder paymentDetails = {paymentDetails}/>
         </Route>
         
       </Switch>
